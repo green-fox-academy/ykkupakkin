33d38754579114392cdc653bf5f2615ab4ea0e16
Ykkupakkin <csoda.bence@gmail.com>
2019-12-05 17:27:13 +0100

    Parametric avg - You still need the avg.

---
+++ week-01/day-4/parametric-average.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+// for (mi, meddig, mit)
+let sum:number=0;
+
+for(let i=1;i<=5;i++) {
+    sum=i+sum;
+}
+
+console.log("Sum: "+ sum);
+
+let avg:number=0;
+
+for(let i=1;0;0) {
+    // avg calculation
+}
