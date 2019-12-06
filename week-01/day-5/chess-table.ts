c4a998cc04bfe51bc8b1d7ed88142510a6c38bc2
Ykkupakkin <csoda.bence@gmail.com>
2019-12-06 17:59:12 +0100

    did the thing, do it more.

--- week-01/day-5/chess-table.ts
+++ week-01/day-5/chess-table.ts
@@ -12,4 +12,17 @@
 //  % % % %
 //
 
+let lineNum: number = 8;
 
+for(let i:number = 0; i <= lineNum; i++) {
+    let j:string = ''
+    for(let k:number = 0; k < lineNum; k++) {
+        if((i+k) % 2 !== 0 ) {
+        j += '0';
+        } else {
+            j +=' ';
+        }
+    }
+
+    console.log(j)
+}
--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -29,3 +29,17 @@ for (let i = 0; i < lineCount; i++) {
     output = "";
 
 }
+
+for (let i = 0; i < lineCount; i++) { 
+    let myspace = "";
+    for (let s = 0; s < (lineCount - i - 1); s++) { 
+        myspace += " ";
+    }
+    for (let j = 1; j <= 2 * i + 1; j++) {
+        output += "*";
+
+    }
+    console.log(myspace + output);
+    output = "";
+
+}
