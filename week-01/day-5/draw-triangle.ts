057ac3c58f315d9a2964ed94353782791fd40048
Ykkupakkin <csoda.bence@gmail.com>
2019-12-06 10:33:22 +0100

    Let

---
+++ week-01/day-5/draw-triangle.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+let star:string=""
+
+for(let i=1;i<=lineCount;i++) {
+    star=star+"*";
+
+    console.log(star);
+}
