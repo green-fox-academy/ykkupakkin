60b4eac24072a1d58096c511b505752b9ce9ef03
Ykkupakkin <csoda.bence@gmail.com>
2019-12-07 12:32:07 +0100

    Campokanak mutattam mi meno mar ez az egesz

--- week-01/day-5/draw-pyramid.ts
+++ week-01/day-5/draw-pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 4;
+
 
 // Write a program that draws a
 // pyramid like this:
@@ -12,24 +12,10 @@ let lineCount: number = 4;
 //
 // The pyramid should have as many lines as lineCount is
 
-/* let pyramid:string=""
-
- for(let i=1;i<=lineCount;i++) {
-     pyramid=pyramid+"*";
-
-     // and decrement the number of space
-
- for(let a=0; a<lineCount;a++) {
-         let space:string="";
-         
- }
-
-     console.log(pyramid);
- }  */
-
-let output = "";          //we set an empty output string
+let lineCount: number = 4;
+let output:string = "";          //we set an empty output string
 for (let i = 0; i < lineCount; i++) { //check if we reached the linecount | why do we define the variable empty?
-    let myspace = "";
+    let myspace:string = "";
     for (let s = 0; s < (lineCount - i - 1); s++) { // we set a number because its easier than string, 
         myspace += " ";
     }
