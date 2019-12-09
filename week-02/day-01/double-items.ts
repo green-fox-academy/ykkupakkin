036720b9b1316f61c91e79c69c965895f0fb2555
Ykkupakkin <csoda.bence@gmail.com>
2019-12-09 17:31:34 +0100

    WhiskeyWhiskeyWhiskeyWhiskeyWhiskeyWhiskey

--- week-02/day-01/double-items.ts
+++ week-02/day-01/double-items.ts
@@ -2,9 +2,12 @@
 // -  Double all the strings in the array, use a built in array method instead of a loop
 // It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
 
-let drinks:string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
 
-//console.log(drinks.join(drinks[0])); -- this might be some part of the logic but this only puts the first item
 
-let allDrinks: string[] = [drinks.indexOf[0] + drinks];
 
+for (let i: number = 0; i < drinks.length; i++) {
+    drinks[i] = drinks[i] + drinks[i];
+    
+}
+console.log(drinks);
