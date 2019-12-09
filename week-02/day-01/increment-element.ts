8b720656204df6a50fbab34fd1ed9ab09e33c0b5
Ykkupakkin <csoda.bence@gmail.com>
2019-12-09 13:48:53 +0100

    Increment element. Spooky stuff

---
+++ week-02/day-01/increment-element.ts
@@ -0,0 +1,8 @@
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+
+let numList:number[] = [1, 2, 3, 4, 5];
+//let incNum: number = 0;
+//incNum += numList[2]++; -- It's weird atm that why did the stuff incremented my value if I didn't even refer to it.
+console.log(numList[2]+1);
--- week-02/day-01/swap-elements.ts
+++ week-02/day-01/swap-elements.ts
@@ -2,6 +2,9 @@
 // -  Swap the first and the third element of `abc`
 
 let abc:string[] = ["Arthur", "Boe", "Chloe"];
-//[abc[2]= abc[0], abc[0] = abc[2]];
-// this method won't work because at the second part you are trying to call a value that has been already changed
-console.log(abc[0], abc[2]);
+let i:string = abc[0];
+
+i = abc[2];
+abc[2] = abc[0];
+abc[0] = i; 
+console.log(abc);
