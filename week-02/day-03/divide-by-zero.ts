991f36baebfb7885c29ba0981df34bb917e61c08
Ykkupakkin <csoda.bence@gmail.com>
2019-12-11 14:14:24 +0100

    Installed some node stuff and I just had to commit.

---
+++ week-02/day-03/divide-by-zero.ts
@@ -0,0 +1,21 @@
+'use strict';
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+// let inputNum: number = 0;
+
+// try {
+// function divide(inputNum: number) : number {  This doesn't work because the function is never called 
+//     let divided: number = 10 / inputNum;         and it only checks if the input value is 0 or not.
+//     console.log(divide);
+//     return divided;
+// }
+// console.log(inputNum + " is not zero.");
+// }
+// catch(e) {
+// console.log('This is the custom');
+// console.log(e);
+// } if (inputNum === 0){
+//     throw Error ('fail');
+// } 
