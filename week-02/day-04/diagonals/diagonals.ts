b974fffb9b484413acaaa24ae72b62ffbd270e91
Ykkupakkin <csoda.bence@gmail.com>
2019-12-12 11:43:36 +0100

    Did the draw. Draw the thing.

---
+++ week-02/day-04/diagonals/diagonals.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const heigth = 400;
+const width = 600;
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+ctx.strokeStyle = "red";
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(width, heigth);
+ctx.stroke();
