fdd022fc63849dffac73b2e1af60ccccf8754661
Ykkupakkin <csoda.bence@gmail.com>
2019-12-17 17:44:01 +0100

    Yeay. Pokemon, nanananaaaanaaaaaaa

---
+++ week-03/day-02/thing.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+class Thing {
+    private name: string;
+    private completed: boolean;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    public complete(): void {
+      this.completed = true;
+    }
+  
+    getName(): string {
+      return this.name;
+    }
+  
+    getCompleted(): boolean {
+      return this.completed;
+    }
+  }
+  
+  export { Thing };
