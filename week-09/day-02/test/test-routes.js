10b5fe67710ee8183671b6b81e814f3804c21f60
[ykkupakkin] <[csoda.bence@gmail.com]>
2020-02-18 15:25:50 +0100

    feat test

--- week-09/day-02/test/test-routes.js
+++ week-09/day-02/test/test-routes.js
@@ -1,13 +1,20 @@
 'use strict';
 
-const request = require('supertest');
-const app = require('./app');
+const request = require('supertest')
+const app = require('./app')
+const { expect } = require('chai');
 
-request(app)
-  .get('/groot')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '15')
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
+describe('GET /groot', () => {
+  it('should return a valid user', (done) => {
+    request(app)
+      .get('/groot')
+      .expect(200)
+      .end((err, res) => {
+        expect(err).to.be.null;
+
+        expect(res.body.message).to.equal('I am Groot!');
+
+        done();
+      });
   });
+});
