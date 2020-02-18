efc52db7a5e3e2e936fc2addbe88817d8b8bb99a
[ykkupakkin] <[csoda.bence@gmail.com]>
2020-02-18 17:15:59 +0100

    feat(second test) done testing. Weird. But good weird.

--- week-09/day-02/test/test-routes.js
+++ week-09/day-02/test/test-routes.js
@@ -12,8 +12,24 @@ describe('GET /groot', () => {
       .expect(404)
       .end((err, res) => {
         expect(err).to.be.null;
-        expect(res.text).to.equal(`{"error":"I am Groot!"}`);
-        //expect(res.json).to.equal({error:"I am Groot!"});
+        //expect(res.text).to.equal(`{"error":"I am Groot!"}`);
+        expect(res.body).to.deep.equal({error:"I am Groot!"});
+
+        done();
+      });
+  });
+
+  it('should return translated I am Groot! instead of alma', (done) => {
+    request(app)
+      .get('/groot')
+      .query({ message: 'alma'})
+      .expect(200)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body).to.deep.equal({
+          received: 'alma',
+          translated: `I am Groot!`
+        });
 
         done();
       });
