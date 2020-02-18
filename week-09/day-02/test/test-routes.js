9ea4aea6eddfba7b7f5a56958d135f5fa273b90a
[ykkupakkin] <[csoda.bence@gmail.com]>
2020-02-18 17:35:20 +0100

    refactor

--- week-09/day-02/test/test-routes.js
+++ week-09/day-02/test/test-routes.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 
-const request = require('supertest')
-const app = require('../routes')
-const { expect } = require('chai');
+const request = require("supertest");
+const app = require("../routes");
+const { expect } = require("chai");
 
-describe('GET /groot', () => {
-  it('should return I am Groot!', (done) => {
+describe("GET /groot", () => {
+  it("should return I am Groot!", done => {
     request(app)
-      .get('/groot')
-      .query('')
+      .get("/groot")
+      .query("")
       .expect(404)
       .end((err, res) => {
         expect(err).to.be.null;
         //expect(res.text).to.equal(`{"error":"I am Groot!"}`);
-        expect(res.body).to.deep.equal({error:"I am Groot!"});
+        expect(res.body).to.deep.equal({ error: "I am Groot!" });
 
         done();
       });
   });
 
-  it('should return translated I am Groot! instead of alma', (done) => {
+  it("should return translated I am Groot! instead of alma", done => {
     request(app)
-      .get('/groot')
-      .query({ message: 'alma'})
+      .get("/groot")
+      .query({ message: "alma" })
       .expect(200)
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res.body).to.deep.equal({
-          received: 'alma',
+          received: "alma",
           translated: `I am Groot!`
         });
 
