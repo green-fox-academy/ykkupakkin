8bd67ef55792724185f448d2c4984d901aad03a8
[ykkupakkin] <[csoda.bence@gmail.com]>
2020-02-24 16:52:42 +0100

    feat(index.js,index.html) add play/pause,next,previous buttons with working js

---
+++ week-10/public/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let _player = document.getElementById("player"),
+    _playlist = document.getElementById("playlist"),
+    _stop = document.getElementById("stop");
+
+// functions
+function playlistItemClick(clickedElement) {
+    let selected = _playlist.querySelector(".selected");
+    if (selected) {
+        selected.classList.remove("selected");
+    }
+    clickedElement.classList.add("selected");
+
+    _player.src = clickedElement.getAttribute("data-ogg");
+    _player.play();
+}
+
+ function playNext() {
+    let selected = _playlist.querySelector("li.selected");
+    if (selected && selected.nextElementSibling) {
+        playlistItemClick(selected.nextElementSibling);
+    }
+}
+ function playPrevious() {
+    let selected = _playlist.querySelector("li.selected");
+    if (selected && selected.previousElementSibling) {
+        playlistItemClick(selected.previousElementSibling);
+    }
+}
+
+// event listeners
+_stop.addEventListener("click", function () {
+	if(_player.paused){
+    _player.play();
+	}else{
+	_player.pause();
+	}
+});
+
+_player.addEventListener("ended", playNext);
+_playlist.addEventListener("click", function (e) {
+     if (e.target && e.target.nodeName === "LI") {
+        playlistItemClick(e.target);
+    }
+});
